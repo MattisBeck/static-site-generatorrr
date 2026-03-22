9a5739219485d86969a6d34dc7949ffbe5253e9d
Mattis <136255616+MattisBeck@users.noreply.github.com>
2026-03-22 09:36:39 +0100

    published demo website

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from website import copy_from_to, generate_pages_recursive
 import os
+import sys
 
 
 def main():
-    print(os.getcwd())
-    copy_from_to("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1]
+    destination = "docs"
+    copy_from_to("static", destination)
+    generate_pages_recursive("content", "template.html", destination, basepath)
+
 
 if __name__ == "__main__":
     main()
