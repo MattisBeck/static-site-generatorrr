fc4f426ed7fadcd3140466943fee2192e217575e
Mattis <136255616+MattisBeck@users.noreply.github.com>
2026-03-24 12:05:14 +0100

    Finished generator

--- src/website.py
+++ src/website.py
@@ -25,7 +25,7 @@ def extract_title(markdown):
             return line[2:]
     raise ValueError("Markdown file doesnt start with a h1 heading")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, "r") as f:
         source = f.read()
@@ -36,19 +36,21 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(source)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{base_path}')
+    template = template.replace('src="/', f'src="{base_path}')
     destination_directories = os.path.dirname(dest_path)
     if not os.path.exists(destination_directories):
         os.makedirs(destination_directories)
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_path, base_path):
     if not os.path.exists(dir_path_content):
         raise FileNotFoundError(f"dir_path_content:{dir_path_content} does not exist")
     for element in os.listdir(dir_path_content):
         print(f"{dir_path_content}/{element}")
         if os.path.isdir(os.path.join(dir_path_content, element)):
-            generate_pages_recursive(os.path.join(dir_path_content, element), template_path, os.path.join(dest_path, element))
+            generate_pages_recursive(os.path.join(dir_path_content, element), template_path, os.path.join(dest_path, element), base_path)
         else:
             destination_path = Path(os.path.join(dest_path, element)).with_suffix(".html")
-            generate_page(os.path.join(dir_path_content, element), template_path, destination_path)
+            generate_page(os.path.join(dir_path_content, element), template_path, destination_path, base_path)
